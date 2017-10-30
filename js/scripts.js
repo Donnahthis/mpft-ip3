e174303e96acb9ac166bfd9f1fd52dd8731c6aab
Dona Adhiambo <donaathiambo2017@gmail.com>
2017-10-30 14:24:41 +0300

    commit changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,6 @@
-// $(document).ready(function() {
-//     $("form#odd-numbers").submit(function(event) {
-//       event.preventDefault();
-//       var decimalNumbers = parseInt($("input#numbers").val());
-//       var result = decimalNumbers(numbers);
-//       $("#result").text(result);
-//     });
-//   });
-//   var numbers = function(number) {
-//       return false;
-//     };
-//     var decimalNumbers = function(number) {
-//         if ((number % 3 === 0) && (number % 5 !== 0) || (number % 15 === 0)) {
-//           return true;
-//         } else {
-//           return false;
-//         }
-//       };
-//
-//       // user interface logic
-//       $(document).ready(function() {
-//         $("form#decimalNumbers").submit(function(event) {
-//           event.preventDefault();
-//           var number = parseInt($("input#number").val());
-//           var result = decimalNumbers(number);
-//
-//           $(".number").text(number);
-//
-//           if (!result) {                 // same as writing if (result === false)
-//             $(".ping").text("pong");
-//           } else {
-//             $(".pingpong").text("");
-//           }
-//
-//           $("#result").show();
-//         });
-//       });
 
-var pingpongGame = function(userNumber) {
+
+var person = function(userNumber) {
   var numberArray = [];
   if (userNumber < 0) { //negative numbers
     for (var index = -1; index >= userNumber; index--) {
@@ -44,7 +8,7 @@ var pingpongGame = function(userNumber) {
     }
     numberArray.forEach(function(number) {
       if (number % 15 === 0) {
-        numberArray[-number-1] = 'pingpongGame';
+        numberArray[-number-1] = 'person';
       } else if (number % 3 === 0) {
         numberArray[-number-1] = 'ping';
       } else if (number % 5 === 0) {
@@ -57,7 +21,7 @@ var pingpongGame = function(userNumber) {
       }
       numberArray.forEach(function(number) {
         if (number % 15 === 0) {
-          numberArray[number-1] = 'pingpong';
+          numberArray[number-1] = 'person';
         } else if (number % 3 === 0) {
           numberArray[number-1] = 'ping';
         } else if (number % 5 === 0) {
@@ -71,10 +35,10 @@ var pingpongGame = function(userNumber) {
 var displayResultList = function(outputArray) {
     $('ul#result').empty();
     on() {
-  $('form#pingpongGame').submit(function(event) {
+  $('form#person').submit(function(event) {
     var userNumber = $('input#userNumber').val();
 
-   displayResultList(pingpongGame(userNumber));
+   displayResultList(person(userNumber));
 
    event.preventDefault();
   });
